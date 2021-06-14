e48258dc07946a21cd992eb5e9fdf42b652081b7
Maria Vlad <maaria.vld@gmail.com>
2021-06-14 17:52:15 +0200

    users validations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+  belongs_to :users
   validates :name, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :lists
+  validates :first_name, :last_name, :username, :email, :encrypted_password, presence: true
+  validates :username, length: { minimum: 4 }, uniqueness: true
+  validates :encrypted_password, length: { minimum: 6}
+  validate :password_requirements_are_met
+  validates :email, uniqueness: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: "valid email please"
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def password_requirements_are_met
+  rules = {
+    " must contain at least one lowercase letter"  => /[a-z]+/,
+    " must contain at least one uppercase letter"  => /[A-Z]+/,
+    " must contain at least one digit"             => /\d+/,
+    " must contain at least one special character" => /[^A-Za-z0-9]+/
+  }
+
+  rules.each do |message, regex|
+    errors.add( :encrypted_password, message ) unless encrypted_password.match( regex )
+  end
+end
 end
